b8f3b1ad1e7cd6349b9c0f333acfe74c3f1da4e8
Thanawatkky <wiko848287@gmail.com>
2024-07-12 14:23:54 +0700

    [change connect database]

--- api/connect.php
+++ api/connect.php
@@ -1,5 +1,5 @@
 <?php 
-   $conn = new mysqli("localhost","root","","db_document");
+   $conn = new mysqli("localhost","root","","db_docfinal");
    $conn->set_charset("utf8");
    ini_set('display_errors',1);
 ?>
